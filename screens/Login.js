293835b259bc5bb063d755792e9c7e4b656b2e1e
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2026-05-11 19:15:22 -0400

    Adicionado login com biometria

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,8 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import * as LocalAuthentication from "expo-local-authentication";
 import { useNavigation } from "@react-navigation/native";
+import * as SecureStore from 'expo-secure-store';
 
 import pb from '../services/pocketbase';
 
@@ -9,24 +10,113 @@ export default function Login({ navigation }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
+  useEffect(() => {
+    verificarSessao();
+  }, []);
+
+  async function verificarSessao() {
+    try {
+      const token = await SecureStore.getItemAsync("token");
+
+      if (!token) return;
+
+      const autenticado = await LocalAuthentication.authenticateAsync({
+                                                    promptMessage:
+                                                      "Entrar com biometria",
+                                                  });
+
+      if (!autenticado.success) return;
+
+      pb.authStore.save(
+        token,
+        null
+      );
+
+      await pb.collection("users").authRefresh();
+
+      navigation.navigate("Home");
+    } catch(error) {
+      console.log(error);
+      pb.authStore.clear();
+      await SecureStore.deleteItemAsync(
+        "token"
+      );
+    }
+  }
+
   async function handleLogin() {
     if (!email || !senha) {
       Alert.alert("Error","Preencha todos os campos!");
       return;
     }
 
-    try{
-      const authData = await pb
-        .collection("users")
-        .authWithPassword(email, senha);
+    try {
+      const authData = await pb.collection("users").authWithPassword(email, senha);
+
+      await SecureStore.setItemAsync(
+        "email",
+        email
+      );
+
+      await SecureStore.setItemAsync(
+        "token",
+        pb.authStore.token
+      );
+
+      await SecureStore.setItemAsync(
+        "user",
+        JSON.stringify(authData.record)
+      );
 
       Alert.alert("Sucesso", "Logado!");
       navigation.navigate("Home");
-      console.log(authData);
-    }catch(error){
+    } catch(error) {
       Alert.alert("Erro", "Login inválido");
-      console.log("ERRO COMPLETO:", error);
-      console.log("ERRO DATA:", error?.data);
+      console.log(error);
+    }
+  }
+
+  async function loginBiometrico() {
+
+    const compativel =
+      await LocalAuthentication
+        .hasHardwareAsync();
+
+    if (!compativel) {
+      Alert.alert("Erro", "Celular sem biometria");
+      return;
+    }
+
+    const autenticado = await LocalAuthentication.authenticateAsync({
+                                                  promptMessage:
+                                                    "Entrar com biometria",
+                                                });
+
+    if (!autenticado.success) {
+      Alert.alert("Erro", "Biometria inválida");
+      return;
+    }
+
+    const token = await SecureStore.getItemAsync("token");
+
+    if (!token) {
+      Alert.alert("Erro", "Nenhuma sessão salva");
+      return;
+    }
+
+    try {
+      pb.authStore.save(
+        token,
+        null
+      );
+
+      await pb.collection("users").authRefresh();
+
+      Alert.alert("Sucesso", "Login biométrico realizado");
+      navigation.navigate("Home");
+    } catch(error) {
+      pb.authStore.clear();
+      Alert.alert("Erro", "Sessão expirada");
     }
   }
 
@@ -51,6 +141,10 @@ export default function Login({ navigation }) {
 
       <TouchableOpacity style={styles.botao} onPress={handleLogin}>
         <Text style={styles.botaoTexto}>Entrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.botao} onPress={loginBiometrico}>
+        <Text style={styles.botaoTexto}>Entrar com biometria</Text>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.navigate("Cadastro Usuário")}>
