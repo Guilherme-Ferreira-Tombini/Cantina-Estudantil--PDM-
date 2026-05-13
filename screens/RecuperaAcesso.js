f2740cf3e840b9f4efd8f6fb01f253c612da3c33
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2026-05-13 17:20:16 -0400

    Modificado recuperação do acesso para envio dos emails para recuperar nova senha

--- screens/RecuperaAcesso.js
+++ screens/RecuperaAcesso.js
@@ -1,61 +1,29 @@
 import React, { useState } from "react";
-
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  Alert
-} from "react-native";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from "react-native";
 
 import pb from "../services/pocketbase";
 
 export default function RecuperaAcesso({ navigation }) {
-
   const [email, setEmail] = useState("");
 
   async function recuperarAcesso() {
-
-    // validar email
     if (!email) {
-
-      Alert.alert(
-        "Erro",
-        "Digite o email"
-      );
-
+      Alert.alert("Erro", "Digite o email");
       return;
     }
 
     try {
-      await pb.collection("users")
-        .requestPasswordReset(email);
-
-      Alert.alert(
-        "Sucesso",
-        "Email de recuperação enviado"
-      );
-
-      navigation.navigate(
-        "Login"
-      );
-
+      await pb.collection("users").requestPasswordReset(email);
+      Alert.alert("Sucesso", "Email de recuperação enviado");
+      navigation.navigate("Login");
     } catch(error) {
-
       console.log(error);
-
-      Alert.alert(
-        "Erro",
-        "Falha ao recuperar acesso"
-      );
+      Alert.alert("Erro", "Falha ao recuperar acesso");
     }
   }
 
   return (
-
     <View style={styles.container}>
-
       <Text style={styles.titulo}>
         Recuperar Acesso
       </Text>
@@ -81,14 +49,12 @@ export default function RecuperaAcesso({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     padding: 20,
     justifyContent: "center",
     backgroundColor: "#228B22",
   },
-
   titulo: {
     fontSize: 24,
     marginBottom: 30,
@@ -96,7 +62,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
-
   input: {
     width: "100%",
     backgroundColor: "#fff",
@@ -104,7 +69,6 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     borderRadius: 8,
   },
-
   botao: {
     width: "100%",
     backgroundColor: "#f3772a",
@@ -113,7 +77,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 20,
   },
-
   botaoTexto: {
     color: "#fff",
     fontWeight: "bold",
